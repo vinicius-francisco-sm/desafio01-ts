40ffffc7ef9b98646b73f3bb2bdd4a7e39175d9d
vinicius-francisco-sm <viniciussiqueira166@gmail.com>
2024-04-06 20:45:19 -0300

    feat: name/accountNumber to readonly + deposit method

--- app.ts
+++ app.ts
@@ -1,9 +1,7 @@
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const peopleAccount: PeopleAccount = new PeopleAccount(1,'Pedrinho', 1);
+console.log(peopleAccount);
+peopleAccount.deposit(20);
+console.log(peopleAccount);
--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,7 +1,7 @@
 export abstract class DioAccount {
-  private name: string
+  private readonly name: string
   private readonly accountNumber: number
-  balance: number = 0
+  private balance: number = 0
   private status: boolean = true
 
   constructor(name: string, accountNumber: number){
@@ -9,22 +9,19 @@ export abstract class DioAccount {
     this.accountNumber = accountNumber
   }
 
-  setName = (name: string): void => {
-    this.name = name
-    console.log('Nome alterado com sucesso!')
-  }
-
   getName = (): string => {
     return this.name
   }
 
-  deposit = (): void => {
+  deposit = (value: number): void => {
     if(this.validateStatus()){
-      console.log('Voce depositou')
+      this.balance += value
+      console.log('Valor depositado com sucesso')
+      console.log('Saldo atual:', this.balance)
     }
   }
 
-  withdraw = (): void => {
+  withdraw = (): void => { // <--------------------
     console.log('Voce sacou')
   }
 
