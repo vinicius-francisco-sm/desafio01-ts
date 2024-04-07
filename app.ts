12bc8c45ffa510f5d4f03730b42e1cd9c47f965b
vinicius-francisco-sm <viniciussiqueira166@gmail.com>
2024-04-06 21:42:10 -0300

    refactor: app.ts instances

--- app.ts
+++ app.ts
@@ -3,17 +3,23 @@ import { PeopleAccount } from './class/PeopleAccount'
 import { PlusAccount } from './class/PlusAccount';
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1,'Pedrinho', 1);
-// console.log(peopleAccount);
-// peopleAccount.deposit(20);
-// peopleAccount.withdraw(21);
-// console.log(peopleAccount);
+console.log(peopleAccount);
+console.log(peopleAccount.getName());
+peopleAccount.deposit(100);
+peopleAccount.withdraw(50);
+peopleAccount.getBalance();
 
 const companyAccount: CompanyAccount = new CompanyAccount('Empresa top', 2);
-// console.log(companyAccount);
-// companyAccount.getLoan(20);
-// console.log(companyAccount);
+console.log(companyAccount);
+console.log(companyAccount.getName());
+companyAccount.deposit(100);
+companyAccount.withdraw(50);
+companyAccount.getBalance();
+companyAccount.getLoan(100);
 
 const plusAccount: PlusAccount = new PlusAccount('Rogerinho', 3);
 console.log(plusAccount);
+console.log(plusAccount.getName());
 plusAccount.deposit(100);
-console.log(plusAccount);
+plusAccount.withdraw(50);
+plusAccount.getBalance();
